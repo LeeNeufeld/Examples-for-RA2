6953458764d42299b73c970f52b89c53571db911
Lee Neufeld <leeneufeld23@hotmail.com>
2019-05-15 19:05:59 -0600

    Add files via upload

---
+++ Neufeld_Lee_Lab3/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Neufeld_Lee_Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                this.Validate();
+                this.productsBindingSource.EndEdit();
+                this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+            }
+            catch(DBConcurrencyException)
+            {
+                MessageBox.Show("Another user updated the same row, " + " Try Again " + " Concurrency Problem ");
+                this.productsTableAdapter.Fill(this.northwindDataSet.Products);// reload data
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show("Other error while saving data: " + ex.Message, ex.GetType().ToString());
+               
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Other error while saving data: " + ex.Message, ex.GetType().ToString());
+            }
+        }
+        private void productsDataGridView_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            int row = e.RowIndex + 1;
+            int col = e.ColumnIndex + 1;
+            MessageBox.Show("Data in the grid in row: " + row + " and column " + col, "Data Error");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Order_Details' table. You can move, or remove it, as needed.
+            this.order_DetailsTableAdapter.Fill(this.northwindDataSet.Order_Details);
+            // TODO: This line of code loads data into the 'northwindDataSet.Categories' table. You can move, or remove it, as needed.
+            this.categoriesTableAdapter.Fill(this.northwindDataSet.Categories);
+            // TODO: This line of code loads data into the 'northwindDataSet.Suppliers' table. You can move, or remove it, as needed.
+            this.suppliersTableAdapter.Fill(this.northwindDataSet.Suppliers);
+            // TODO: This line of code loads data into the 'northwindDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
